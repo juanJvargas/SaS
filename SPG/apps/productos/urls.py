4e902c12c0f9560ce5a8c95e496e9dc9c303f2fd
juanJvargas <juan.jose.vargas@correounivalle.edu.co>
2021-01-11 22:14:03 -0500

    Anadir y listar productos

--- SPG/apps/productos/urls.py
+++ SPG/apps/productos/urls.py
@@ -6,7 +6,13 @@ from apps.productos import views
 app_name = 'productos'
 
 urlpatterns = [
+    path('listar-productos', views.ProductosListar.as_view(), name='productos_listar'),
+    path('crear-producto', views.ProductoCrear.as_view(), name='producto_crear'),
+
     path('listar-ingredientes', views.IngredientesListar.as_view(), name='ingredientes_listar'),
     path('crear-ingrediente', views.IngredienteCrear.as_view(), name='ingrediente_crear'),
      
+
+    path('nuestro-menu/', views.Tienda.as_view(template_name='productos/menu.html'), name='menu'),
+    path('tienda/', views.Tienda.as_view(template_name='productos/tienda.html'), name='tienda'),
 ]
--- SPG/apps/productos/views.py
+++ SPG/apps/productos/views.py
@@ -38,3 +38,56 @@ class IngredienteCrear(SuccessMessageMixin, CreateView):
     def get_success_url(self, **kwargs):
         return reverse_lazy("productos:ingredientes_listar")
 
+
+
+
+class ProductosListar(ListView): 
+    model = Producto
+
+    def get_context_data(self, **kwargs):
+        context = super(ProductosListar, self).get_context_data(**kwargs)
+        context['usuario'] = self.request.user
+        schema = connection.schema_name
+        context['tenant'] = Tenant.objects.get(schema_name=schema)
+        return context
+
+
+
+class ProductoCrear(SuccessMessageMixin, CreateView):
+    model = Producto
+    form_class = ProductoForm
+    success_message = 'Producto agregado con exito'
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+
+        total = form.cleaned_data.get('precio')
+        for ing in form.cleaned_data.get('ingredientes'):
+            total += ing.precio_por_unidad
+
+        form.instance.precio = total
+        return super(ProductoCrear, self).form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        context = super(ProductoCrear, self).get_context_data(**kwargs)
+        context['usuario'] = self.request.user
+        schema = connection.schema_name
+        context['tenant'] = Tenant.objects.get(schema_name=schema)
+        return context
+
+    def get_success_url(self, **kwargs):
+        return reverse_lazy("productos:productos_listar")
+
+class Tienda(ListView):
+    model = Producto
+
+    def get_queryset(self):
+        return Producto.objects.filter(estado=True, cantidad__gte=1)
+
+    def get_context_data(self, **kwargs):
+        context = super(Tienda, self).get_context_data(**kwargs)
+        context['usuario'] = self.request.user
+        schema = connection.schema_name
+        context['tenant'] = Tenant.objects.get(schema_name=schema)
+        context['orden'] = get_orden_usuario_pendiente(self.request)
+        return context
